8d76ff7d464e9e1da84a37b9f128a8698b1c28b2
Nathan <antnefedov33@gmail.com>
2023-12-18 13:55:31 +0300

    made the base of the 3d scroll

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,25 @@
 "use strict"
+
+let frames = document.querySelectorAll('.frame');
+let zSpacing = -1000;
+let lastPos = zSpacing / 5;
+let zValues = [];
+
+window.addEventListener('scroll', () => {
+    let top = document.documentElement.scrollTop;
+    let delta = lastPos - top;
+
+    lastPos = top;
+
+    frames.forEach((elem, index) => {
+        zValues.push(index * zSpacing + zSpacing);
+        zValues[index] += delta * -5.5;
+
+        let frame = frames[index];
+        let transform = `translateZ(${zValues[index]}px)`;
+        let opacity = zValues[index] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
+        frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+    })
+})
+
+window.scrollTo(0, 1)
